f68e03196aed0d5fa7a1e7bac34874ec245e9aae
Basuki Winoto <basuki@app-robot.com>
2023-09-16 11:14:18 -0400

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { ActivityIndicator, Button, StyleSheet, Text, View } from 'react-native';
+import { Camera, useCameraDevices, useFrameProcessor } from 'react-native-vision-camera';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  const [permission, setPermission] = useState(false);
+  const devices = useCameraDevices();
+  console.log({devices})
+  const device = devices.back;
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  useEffect(() => {
+    (async () => {
+      const cameraPermission = await Camera.getCameraPermissionStatus();
+      if (cameraPermission !== 'granted' ) {
+        const newCameraPermission = await Camera.requestCameraPermission();
+        setPermission(newCameraPermission === 'granted');
+        return;
+      }
+      setPermission(cameraPermission === 'granted');
+    })()
+  }, []);
+
+  const frameProcessor = useFrameProcessor((frame) => {
+    'worklet'
+    const isHotdog = detectIsHotdog(frame)
+    console.log(isHotdog ? "Hotdog!" : "Not Hotdog.")
+  }, [])
+
+  if (device == null) return <View style={{flex:1, justifyContent: 'center', alignItems: 'center'}}><ActivityIndicator /></View>;
+  return (
+    <Camera
+      style={StyleSheet.absoluteFill}
+      device={device}
+      isActive={true}
+      frameProcessor={frameProcessor}
+    />
+  )
+};
